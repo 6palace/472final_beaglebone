c1d9f4522c59b54bccc8d41b968c8eb1de2442d0
Lab <lab@example.com>
2015-12-01 18:38:57 -0800

    The interrupt for adc is working

--- lab3Playground/adc.c
+++ lab3Playground/adc.c
@@ -9,7 +9,7 @@
 #include <signal.h>
 #include <stdlib.h>
 
-#define TIMER_TAG 77
+#define TIMER_TAG 263 //Unique number for timer signal identifier
 
 void timeHandler(int sigNo, siginfo_t * evp, void * ucontext);
 
@@ -17,51 +17,37 @@ FILE* adcOut;
 
 int main() {
 
-	   srand(time(NULL));
+   srand(time(NULL));
+   mknod("/tmp/adcData", S_IFIFO, 0);
+   adcOut = fopen("/tmp/adcData", "w");
 
-   
-    struct sigevent    sigx;
-	struct sigaction   sigAct;
-	struct itimerspec  newTime;
-	timer_t timer;
-   // sigx.sigev_notify = SIGEV_SIGNAL;
-	sigemptyset(&sigAct.sa_mask);
-	sigAct.sa_flags = 0;
-	sigAct.sa_handler = (void (*))timeHandler;
+   struct sigevent    sigx;
+   struct sigaction   sigAct;
+   struct itimerspec  newTime;
+   timer_t timer;
+
+   sigemptyset(&sigAct.sa_mask);
+   sigAct.sa_flags = 0;
+   sigAct.sa_handler = (void (*))timeHandler;
    sigaction(SIGUSR1, &sigAct, 0);
 
    sigx.sigev_notify          = SIGEV_SIGNAL;
-    sigx.sigev_signo           = SIGUSR1;
-    sigx.sigev_value.sival_int = TIMER_TAG;
+   sigx.sigev_signo           = SIGUSR1;
+   sigx.sigev_value.sival_int = TIMER_TAG;
 
-    //expires in 1 second and every 3 seconds after
-    newTime.it_value.tv_sec     = 1; 
-    newTime.it_value.tv_nsec    = 0;
-    newTime.it_interval.tv_sec  = 3;
-    newTime.it_interval.tv_nsec = 0;
+   //expires in 1 second and every 3 seconds after
+   newTime.it_value.tv_sec     = 0; 
+   newTime.it_value.tv_nsec    = 1;
+   newTime.it_interval.tv_sec  = 0;
+   newTime.it_interval.tv_nsec = 100000000;
 
-    //clock_gettime(CLOCK_REALTIME, &newTime);
-    //future.it_value.tv_sec = d.tv
+   timer_create(CLOCK_REALTIME, &sigx, &timer);
+   timer_settime(timer, 0,  &newTime, NULL);
 
+   while(1)
+      pause();
 
-	mknod("/tmp/adcData", S_IFIFO, 0);
-	adcOut = fopen("/tmp/adcData", "w");
-	
-	timer_create(CLOCK_REALTIME, &sigx, &timer);
-    timer_settime(timer, 0,  &newTime, NULL);
-//		fprintf(adcOut, "%d,%d,%d,%d\n", readADC(4), readADC(5), readADC(6), readADC(7));
-		
-		// if(timer_settime(timer, 0, 1, NULL) == ;) {
-		// 	fflush(adcOut);
-		// }
-
-sigset_t mask;
-sigprocmask(0, NULL, &mask);
-sigdelset(&mask, SIGVTALRM);
-sigsuspend(&mask);
-
-  //  pause();
-	return 0;
+   return 0;
 }
 
 int readADC(int whichADC) {
@@ -80,7 +66,6 @@ int readADC(int whichADC) {
 
 
 void timeHandler(int sigNo, siginfo_t * evp, void * ucontext) {
-	time_t timer = time(0);
 	printf("called\n");
 	fprintf(adcOut, "%d,%d,%d,%d\n", readADC(4), readADC(5), readADC(6), readADC(7));
 }
